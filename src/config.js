19c75f4b88c159cb39495f28c3628651f0e50479
EJ132 <ejg132@gmail.com>
2020-01-06 11:15:45 -0800

    final

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
-  API_ENDPOINT: 'https://stormy-river-01603.herokuapp.com/api',
+  API_ENDPOINT: 'http://localhost:8000/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 }
+
+// https://stormy-river-01603.herokuapp.com/api
